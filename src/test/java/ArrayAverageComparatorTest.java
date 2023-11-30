32647d6852960e95c29811a3ffdcd4660651d136
NickolayBelyaev <nickb2147@gmail.com>
2023-11-30 20:23:11 +0300

    модель и тесты

--- src/test/java/ArrayAverageComparatorTest.java
+++ src/test/java/ArrayAverageComparatorTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -12,11 +13,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 // хотя тестировать private методы это достаточно странная история, ведь можно протестировать public методы, которые
-// используют эти private-методы, но мы тут веников не вяжем и знакомимся с понятием рефлексии (Reflection API).
+// используют эти private-методы, но мы тут веников не вяжем, а вяжем тесты, походу знакомясь с понятием рефлексии (Reflection API).
 class ArrayAverageComparatorTest {
-    private ArrayAverageComparator arrayAverageComparator;
+    private final Class<?> reflection = Class.forName("ArrayAverageComparator");
+    private final Object reflectionClass = reflection.getConstructor(int[].class, int[].class).newInstance(new int[]{1, 2, 3}, new int[]{1, 2, 3});
+
+    // ради проброса исключений
+    ArrayAverageComparatorTest() throws
+            ClassNotFoundException, InvocationTargetException, InstantiationException,
+            IllegalAccessException, NoSuchMethodException {}
 
     @ParameterizedTest
+    @DisplayName("Сравнение средних значений")
     @CsvSource({
             "1, 1, 0",
             "2, 1, 1",
@@ -24,29 +32,55 @@ class ArrayAverageComparatorTest {
     })
     void averageComparatorTest(float averageFirst, float averageSecond, int expectedResult) {
         try {
-            Class<?> reflection = Class.forName("ArrayAverageComparator");
-            Object reflectionClass = reflection.getConstructor(int[].class, int[].class).newInstance(new int[]{1, 2, 3}, new int[]{1, 2, 3});
             Method averageComparator = reflectionClass.getClass().getDeclaredMethod("averageComparator", float.class, float.class);
             averageComparator.setAccessible(true);
+
             assertEquals(expectedResult, averageComparator.invoke(reflectionClass, averageFirst, averageSecond));
-        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException |
-                 ClassNotFoundException | InstantiationException e) {
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
             e.printStackTrace();
         }
     }
 
     @Test
+    @DisplayName("Вычисление средних значений")
     void averageCalculatorTest() {
         try {
-            Class<?> reflection = Class.forName("ArrayAverageComparator");
-            Object reflectionClass = reflection.getConstructor(int[].class, int[].class).newInstance(new int[]{1, 2, 3}, new int[]{1, 2, 3});
             Method averageCalculator = reflectionClass.getClass().getDeclaredMethod("averageCalculator", int[].class);
             averageCalculator.setAccessible(true);
 
-            assertEquals((float) 2, averageCalculator.invoke(reflectionClass, (Object) new int[]{1, 2, 3}));
-        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException |
-                 ClassNotFoundException | InstantiationException e) {
+            assertEquals((float) 2, averageCalculator.invoke(reflectionClass, new int[]{1, 2, 3}));
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    @DisplayName("Среднее первого массива больше среднего второго")
+    void averageComparatorTestFirst() {
+        ArrayAverageComparator comparator = new ArrayAverageComparator(new int[]{1, 2, 3}, new int[]{1, 1, 1});
+        assertEquals(1, comparator.getCompareResult());
+    }
+
+    @Test
+    @DisplayName("Среднее второго массива больше среднего первого")
+    void averageComparatorTestSecond() {
+        ArrayAverageComparator comparator = new ArrayAverageComparator(new int[]{1, 1, 1}, new int[]{1, 2, 3});
+        assertEquals(2, comparator.getCompareResult());
+    }
+
+    @Test
+    @DisplayName("Среднее первого и второго массива равны")
+    void averageComparatorTestEquals() {
+        ArrayAverageComparator comparator = new ArrayAverageComparator(new int[]{1, 1, 3}, new int[]{1, 1, 3});
+        assertEquals(0, comparator.getCompareResult());
+    }
+
+    @Test
+    @DisplayName("Один из массивов пуст")
+    void averageComparatorTestIllegalArg() {
+        assertThrows(IllegalArgumentException.class, () ->
+                new ArrayAverageComparator(new int[]{1, 1, 3}, new int[]{}));
+        assertThrows(IllegalArgumentException.class, () ->
+                new ArrayAverageComparator(new int[]{}, new int[]{1, 1, 3}));
     }
 }
